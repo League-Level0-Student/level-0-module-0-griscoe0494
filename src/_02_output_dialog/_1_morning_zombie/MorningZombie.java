a68012d8e5e4587191e1a522cb6b147b82c1c8b5
griscoe0494 <62079302+griscoe0494@users.noreply.github.com>
2020-04-22 22:59:36 +0000

    zombie

---
+++ src/_02_output_dialog/_1_morning_zombie/MorningZombie.java
@@ -0,0 +1,20 @@
+package _02_output_dialog._1_morning_zombie;
+
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+
+public static void main(String[] args) {	
+JOptionPane.showMessageDialog(null,"Wake up");
+JOptionPane.showMessageDialog(null,"Make your bed");
+JOptionPane.showMessageDialog(null,"Eat breakfast");
+JOptionPane.showMessageDialog(null,"Brush teeth, brush hair, and wash face");
+JOptionPane.showMessageDialog(null,"Get Dressed for Online classes");
+JOptionPane.showMessageDialog(null, "Do homework");
+
+}
+}
+	//Add a main method.
+	//Think of a list of instructions for your typical morning routine.
+	//Use a pop-up for each item in your list:
+	//JOptionPane.showMessageDialog(null,"Here is my message");
